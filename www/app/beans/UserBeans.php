374b552eebc82c5420188fd6943bde4b36de259e
torikai <s17ar131@yahoo.co.jp>
2020-06-20 11:32:27 +0900

    ファイル移動

---
+++ www/app/beans/UserBeans.php
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GREET="hello, "
+NAME="torikai"
+
+
+echo "who are you?"
+echo "${GREET}${NAME}!"
+echo "----------------------"
+echo $0
---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/css/index.css">
+</head>
+<body>
+	<h1>Who are you?</h1>
+	<img src="/img/IMG_5829.jpeg"><br>
+	<form action="/signIn.php" method="post">
+		<input class="form" type="text" name="userName" placeholder="User name">
+		<input class="form" type="text" name="password" placeholder="Password">
+		<input type="submit" value="enter">
+</body>
+
